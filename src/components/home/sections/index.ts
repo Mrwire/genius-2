ab08318f5a3c5044313f328d95460dee1e73ee7f
Oussama BENAAMIMI <35456006+Mrwire@users.noreply.github.com>
2025-01-06 19:17:35 +0100

    lk

--- src/components/home/sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Hero } from './Hero';
-export { default as Presentation } from './Presentation';
-export { default as OurServices } from './OurServices';
-export { default as Group } from './Group';
-export { default as Contact } from './Contact'; 
