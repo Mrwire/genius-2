1d84ed683f8f6c1a3a29aaf935903a94bbc64f23
Oussama BENAAMIMI <35456006+Mrwire@users.noreply.github.com>
2025-01-06 19:07:43 +0100

    978

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import SplineSection from '../components/spline/SplineSection';
+import OurServices from '../components/home/sections/OurServices';
 
 export default function Home() {
   return (
     <main>
       {/* Autres sections... */}
       <SplineSection />
+      <OurServices />
       {/* Autres sections... */}
     </main>
   );
